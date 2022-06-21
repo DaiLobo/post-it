e19e35e4ae2a4924350b729542f3666c3475be03
Diana <diana45637@live.com>
2022-06-21 02:18:20 -0300

    feature/components-form-list-button

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from "./componets/Form";
+import List from "./componets/List";
+
+function App() {
+  return (
+    <div>
+      <Form/>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
