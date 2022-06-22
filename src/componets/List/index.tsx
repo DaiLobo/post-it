91c8ea28f553c63d64cea9bec9ec02b0c2768075
Diana <diana45637@live.com>
2022-06-21 23:12:43 -0300

    props-list-item

--- src/componets/List/index.tsx
+++ src/componets/List/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from './style.module.scss'
 
 export default function List () {
@@ -21,14 +22,11 @@ export default function List () {
             <h2> Estudos do dia </h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li key={index} className={style.item}>
-                        <h3>
-                            {item.tarefa}
-                        </h3>
-                        <span>
-                            {item.tempo}
-                        </span>
-                    </li>
+                    <Item
+                        key={index}
+                        { ...item }
+                    />
+
                 ))}
             </ul>
         </aside>
